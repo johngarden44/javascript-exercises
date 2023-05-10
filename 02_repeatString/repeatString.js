17975979f00af31bdb7b797814e9b2845fa35672
John-MB <35427031+johngarden44@users.noreply.github.com>
2023-05-09 21:17:42 -0400

    passed test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,22 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
 
-};
+let copiedWord = "";
 
+    if (num > 0) { 
+for ( let i =0; i < num; i++) {
+    copiedWord = copiedWord + string;
+}
+return copiedWord;
+}
+
+else if (num === 0) {
+    copiedWord = "";
+return copiedWord;
+}
+
+else {
+return "ERROR";
+}
+}
 // Do not edit below this line
 module.exports = repeatString;
