85cf64ac4f85a4036a5b51e04a656bacc977dc81
John-MB <35427031+johngarden44@users.noreply.github.com>
2023-05-10 23:45:24 -0400

    passed test

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(word) {
+let wordLength = word.length;
+let newWord = "";
 
+while (wordLength > 0) {
+newWord = newWord + word.substring(wordLength, wordLength - 1)
+wordLength--;
+}
+return newWord
 };
 
 // Do not edit below this line
